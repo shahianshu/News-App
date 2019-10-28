ca281354721feed39d504f1ea3fe5c93b28d4362
Anshu Shahi <anshushahi@Anshus-MacBook-Air.local>
2019-10-28 19:45:30 +0530

    complete networking using country parameter

--- News App/ViewController.swift
+++ News App/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
+    
+    let api_key = "8764af48e8564aba9eccb02d2daafc1c"
+    let url = "https://newsapi.org/v2/top-headlines?"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getNews(url : url , api_key : api_key)
     }
 
 
 }
+
+
+// MARk: - Networking
+
+// write getNews function
+
+func getNews(url : String , api_key : String ){
+    let urlWithParam = url + "country=in&" + "apiKey=" + api_key 
+    print(urlWithParam)
+    Alamofire.request(urlWithParam).responseJSON { (response) in
+        if response.result.isSuccess {
+            print(response)
+        }
+    }
+    
+
+}
+
 
